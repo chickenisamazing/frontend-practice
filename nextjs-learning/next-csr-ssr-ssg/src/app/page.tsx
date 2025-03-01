aa401656627fff0209344c2b0389b84fc050769e
chickenisamazing <dieswing@naver.com>
2025-03-01 13:17:28 +0900

    style: Modify text

--- nextjs-learning/next-csr-ssr-ssg/src/app/page.tsx
+++ nextjs-learning/next-csr-ssr-ssg/src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.title}>
-      <div className={styles.board}>케아의 렌더링 테스트</div>
+      <div className={styles.board}>정적 라우팅 테스트</div>
     </div>
   );
 }
