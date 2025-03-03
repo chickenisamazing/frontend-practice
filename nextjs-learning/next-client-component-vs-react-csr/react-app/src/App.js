786c37c34183fd96e7aed70e72e6eb65eee7fa12
chickenisamazing <dieswing@naver.com>
2025-03-03 15:47:20 +0900

    feat: Add rendering delay test code in react project

--- nextjs-learning/next-client-component-vs-react-csr/react-app/src/App.js
+++ nextjs-learning/next-client-component-vs-react-csr/react-app/src/App.js
@@ -1,25 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+export default function Home() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setData("푸라닭 고추마요 최고");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{ textAlign: "center", padding: "20px", background: "#e3f2fd" }}
+    >
+      <h1 style={{ margin: "15px", color: "white" }}>React</h1>
+      <h2 style={{ color: "white" }}>Client Side Rendering(CSR)</h2>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          margin: "20px auto",
+          background: data ? "#4caf50" : "#bdbdbd",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          fontSize: "18px",
+        }}
+      >
+        {data ? "실행 완료!" : "로딩 중..."}
+      </div>
     </div>
   );
 }
-
-export default App;
