fd044fefa63296312c4a4ab8c12b4a622b8cbea3
chickenisamazing <dieswing@naver.com>
2025-03-03 15:47:10 +0900

    feat: Add rendering delay test code in next App Router project

--- nextjs-learning/next-client-component-vs-react-csr/next-app/src/app/page.tsx
+++ nextjs-learning/next-client-component-vs-react-csr/next-app/src/app/page.tsx
@@ -1,9 +1,38 @@
 "use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState<string | null>(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setData("푸라닭 고추마요 최고");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      <div>클라이언트 컴포넌트의 렌더링 테스트</div>
+    <div
+      style={{ textAlign: "center", padding: "20px", background: "#000000" }}
+    >
+      <h1 style={{ margin: "15px", color: "white" }}>Next.js - App Router</h1>
+      <h2 style={{ color: "white" }}>Client Component Rendering</h2>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          margin: "20px auto",
+          background: data ? "#7DA3FC" : "#ceafe8",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          fontSize: "18px",
+        }}
+      >
+        {data ? "실행 완료!" : "로딩 중..."}
+      </div>
     </div>
   );
 }
